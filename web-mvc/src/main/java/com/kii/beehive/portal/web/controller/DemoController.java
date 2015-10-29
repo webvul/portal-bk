73d31a797403ce70b8b4c4121790583def75c486
steven.jiang@kii.com <steven.jiang@kii.com>
2015-10-29 14:00:49 +0800

    add ignore

--- web-mvc/src/main/java/com/kii/beehive/portal/web/controller/DemoController.java
+++ web-mvc/src/main/java/com/kii/beehive/portal/web/controller/DemoController.java
@@ -1,5 +1,7 @@
 package com.kii.beehive.portal.web.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +23,14 @@ public class DemoController {
 
 	}
 
+	@RequestMapping(path="/echo",method={RequestMethod.POST},consumes = {"application/json"},produces = {"application/json"})
+	public HelloEntry echo(@RequestBody HelloEntry entry){
 
+//		entry.setName("hello");
+//		entry.setValue("world");
+
+		return entry;
+
+	}
 
 }
