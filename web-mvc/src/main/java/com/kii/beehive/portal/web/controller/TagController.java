3c9c21f58810f3cab196f78143d007ee9c9b8c46
JasonChang <jasnchang@kii.com>
2015-11-09 18:18:48 +0800

    add tag method

--- web-mvc/src/main/java/com/kii/beehive/portal/web/controller/TagController.java
+++ web-mvc/src/main/java/com/kii/beehive/portal/web/controller/TagController.java
@@ -4,33 +4,36 @@ package com.kii.beehive.portal.web.controller;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kii.beehive.portal.manager.ThingManager;
 import com.kii.beehive.portal.store.entity.GlobalThingInfo;
 import com.kii.beehive.portal.store.entity.TagIndex;
-import com.kii.beehive.portal.web.entity.ThingInput;
 
 @RestController
 @RequestMapping(path = "/tags",  consumes = {"application/json"}, produces = {"application/json"})
 public class TagController {
-
+	
+	@Autowired
+	private ThingManager thingManger;
 
 	@RequestMapping(path="/",method={RequestMethod.POST})
 	public Map<String,String> createTag(@RequestBody TagIndex input){
-
+		thingManger.createTag(input);
 		return null;
 
 	}
 
 
 	@RequestMapping(path = "/tag/{tagName}", method = {RequestMethod.GET})
-	public List<GlobalThingInfo> getThingsByTag(@PathVariable("tagName") String tagName) {
-
-		return null;
+	public TagIndex getThingsByTag(@PathVariable("tagName") String tagName) {
+		TagIndex tagIndex = thingManger.findTagIndexByTagName(tagName);
+		return tagIndex;
 
 	}
 
