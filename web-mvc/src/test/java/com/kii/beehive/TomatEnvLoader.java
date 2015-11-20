0d1333b896364d865f6b625a92083b891a74faa4
steven.jiang@kii.com <steven.jiang@kii.com>
2015-11-20 18:04:51 +0800

    add tomcat embedded

---
+++ web-mvc/src/test/java/com/kii/beehive/TomatEnvLoader.java
@@ -0,0 +1,31 @@
+package com.kii.beehive;
+
+import java.io.File;
+
+import org.apache.catalina.WebResourceRoot;
+import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.webresources.DirResourceSet;
+import org.apache.catalina.webresources.StandardRoot;
+
+
+public class TomatEnvLoader {
+
+
+	public static void main( String[] args ) throws Exception
+	{
+
+		System.setProperty("spring.profile","test");
+
+		String webappDirLocation = "./web-mvc/src/main/webapp/";
+		Tomcat tomcat = new Tomcat();
+
+		tomcat.setHostname("localhost");
+		tomcat.setPort(8080);
+
+		tomcat.addWebapp("/beehive-portal", new File(webappDirLocation).getAbsolutePath());
+
+		tomcat.start();
+		tomcat.getServer().await();
+	}
+}
