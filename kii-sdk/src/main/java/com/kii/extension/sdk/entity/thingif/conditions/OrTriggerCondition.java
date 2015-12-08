7ad0562de7e9ead4a4ae7f6f4c5b05e5fb16d454
steven.jiang@kii.com <steven.jiang@kii.com>
2015-12-08 17:32:52 +0800

    finish api

--- kii-sdk/src/main/java/com/kii/extension/sdk/entity/thingif/conditions/OrTriggerCondition.java
+++
@@ -1,9 +0,0 @@
-package com.kii.extension.sdk.entity.thingif.conditions;
-
-public class OrTriggerCondition extends LogicTriggerCondition {
-
-	@Override
-	public ConditionType getType(){
-		return ConditionType.Or;
-	}
-}
