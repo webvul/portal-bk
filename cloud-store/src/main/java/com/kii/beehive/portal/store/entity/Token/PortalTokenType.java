b2ae692732d46bac8bcfb13d62efd81b0e3dda7f
steven.jiang@kii.com <steven.jiang@kii.com>
2015-12-14 15:10:49 +0800

    split store-service

---
+++ cloud-store/src/main/java/com/kii/beehive/portal/store/entity/Token/PortalTokenType.java
@@ -0,0 +1,9 @@
+package com.kii.beehive.portal.store.entity.Token;
+
+
+
+public enum PortalTokenType {
+
+	UserSync,Demo,Admin;
+
+}
---
+++ cloud-store/src/main/java/com/kii/beehive/portal/store/entity/usersync/ExecuteResult.java
@@ -0,0 +1,6 @@
+package com.kii.beehive.portal.store.entity.usersync;
+
+public enum ExecuteResult {
+
+	Success,Working,Finish;
+}
