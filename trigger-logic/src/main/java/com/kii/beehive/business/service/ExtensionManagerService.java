95a6b1d27461b949da4f7f69ec5892c3d3d5856b
steven.jiang@kii.com <steven.jiang@kii.com>
2015-12-22 16:13:33 +0800

    merge from master

---
+++ trigger-logic/src/main/java/com/kii/beehive/business/service/ExtensionManagerService.java
@@ -0,0 +1,15 @@
+package com.kii.beehive.business.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.kii.extension.sdk.service.ServiceExtensionService;
+
+public class ExtensionManagerService {
+
+
+	@Autowired
+	private ServiceExtensionService  extensionService;
+
+
+
+}
