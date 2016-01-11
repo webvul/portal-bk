6a5a2cdec2f2ed260383dd209f54b0ddb9782955
steven.jiang@kii.com <steven.jiang@kii.com>
2016-01-11 15:00:27 +0800

    add cache constant

--- cloud-store/src/main/java/com/kii/beehive/portal/config/CacheConfig.java
+++ cloud-store/src/main/java/com/kii/beehive/portal/config/CacheConfig.java
@@ -1,47 +1,11 @@
 package com.kii.beehive.portal.config;
 
 
-import java.util.concurrent.TimeUnit;
-
-import org.springframework.cache.Cache;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.guava.GuavaCache;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import com.google.common.cache.CacheBuilder;
-
-//@Configuration
-//@EnableCaching
 public class CacheConfig {
 
 	public static final String TTL_CACHE="ttl_cache";
 
 	public static final String LONGLIVE_CACHE="long_live_cache";
 
-//
-//	@Bean
-//	public Cache cacheTTL() {
-//		return new GuavaCache(TTL_CACHE, CacheBuilder.newBuilder()
-//				.expireAfterWrite(60, TimeUnit.MINUTES)
-//				.build());
-//	}
-//
-//
-//	@Bean
-//	public Cache cachePeresie() {
-//		return new GuavaCache(LONGLIVE_CACHE, CacheBuilder.newBuilder()
-//				.expireAfterWrite(60, TimeUnit.MINUTES)
-//				.build());
-//	}
-//
-//	@Bean
-//	public Cache demoCache(){
-//
-//		return new GuavaCache("demo_cache",CacheBuilder.newBuilder()
-//				.expireAfterWrite(10,TimeUnit.SECONDS)
-//				.expireAfterAccess(10,TimeUnit.SECONDS)
-//				.build());
-//	}
 
 }
