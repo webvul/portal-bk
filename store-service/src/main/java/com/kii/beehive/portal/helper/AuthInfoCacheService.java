c6784f9e30c363f422b089b541da1da10d739da1
steven.jiang@kii.com <steven.jiang@kii.com>
2016-01-11 11:33:07 +0800

    fix issue #103 convert to ehcache implement

--- store-service/src/main/java/com/kii/beehive/portal/helper/AuthInfoCacheService.java
+++ store-service/src/main/java/com/kii/beehive/portal/helper/AuthInfoCacheService.java
@@ -7,6 +7,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.http.annotation.NotThreadSafe;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
