2dbf9ca4f0eafeaae8263863531c9228a066f43c
steven.jiang@kii.com <steven.jiang@kii.com>
2016-02-22 17:41:57 +0800

    fix ParamNotFound exception bug #146

--- kii-sdk/src/main/java/com/kii/extension/sdk/exception/AppParameterCodeNotFoundException.java
+++ kii-sdk/src/main/java/com/kii/extension/sdk/exception/AppParameterCodeNotFoundException.java
@@ -1,8 +1,15 @@
 package com.kii.extension.sdk.exception;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 public class AppParameterCodeNotFoundException extends KiiCloudException {
 
 	private String parameterName;
+
+	@JsonIgnore
+	public int getStatusCode() {
+		return 404;
+	}
 
 	public String getParameterName() {
 		return parameterName;
