6b65ec036a4409bb203caa642c95dc667ee194de
Luan <147530144@qq.com>
2016-03-24 22:38:53 +0800

    cn kanban  #76 add Beehive API for inquiry of device supplier info

--- web-mvc/src/main/java/com/kii/beehive/portal/web/aop/ControllerLogAspect.java
+++ web-mvc/src/main/java/com/kii/beehive/portal/web/aop/ControllerLogAspect.java
@@ -102,6 +102,7 @@ public class ControllerLogAspect {
             try {
                 return mapper.writeValueAsString(obj);
             } catch (JsonProcessingException e) {
+                log.error(e.getMessage(), e);
                 return "";
             }
 
