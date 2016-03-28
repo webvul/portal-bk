99c4f56fdea7badbf824691944720881b90f0fab
hdchen <hdchen.tw@gmail.com>
2016-03-28 16:58:01 +0800

    format source code.

--- jdbc-store/src/main/java/com/kii/beehive/portal/jdbc/dao/TagGroupRelationDao.java
+++ jdbc-store/src/main/java/com/kii/beehive/portal/jdbc/dao/TagGroupRelationDao.java
@@ -1,46 +1,43 @@
 package com.kii.beehive.portal.jdbc.dao;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import com.kii.beehive.portal.jdbc.entity.TagGroupRelation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
-import com.kii.beehive.portal.jdbc.entity.GroupUserRelation;
-import com.kii.beehive.portal.jdbc.entity.TagGroupRelation;
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public class TagGroupRelationDao extends SpringBaseDao<TagGroupRelation> {
 
-	private Logger log= LoggerFactory.getLogger(TagGroupRelationDao.class);
-	
 	public static final String TABLE_NAME = "rel_tag_group";
 	public static final String KEY = "id";
-	
-	public void delete(Long tagID, Long userGroupID){
-		if(tagID != null || userGroupID != null){
+	private Logger log = LoggerFactory.getLogger(TagGroupRelationDao.class);
+
+	public void delete(Long tagID, Long userGroupID) {
+		if (tagID != null || userGroupID != null) {
 			String sql = "DELETE FROM " + this.getTableName() + " WHERE ";
-			
+
 			StringBuilder where = new StringBuilder();
 			List<Object> params = new ArrayList<Object>();
-			if(tagID != null){
-				where.append(TagGroupRelation.TAG_ID + " = ? "); 
+			if (tagID != null) {
+				where.append(TagGroupRelation.TAG_ID + " = ? ");
 				params.add(tagID);
 			}
-			
-			if(userGroupID != null){
-				if(where.length() > 0){
+
+			if (userGroupID != null) {
+				if (where.length() > 0) {
 					where.append(" AND ");
 				}
-				where.append(TagGroupRelation.USER_GROUP_ID+" = ? ");
+				where.append(TagGroupRelation.USER_GROUP_ID + " = ? ");
 				params.add(userGroupID);
 			}
 			Object[] paramArr = new Object[params.size()];
 			paramArr = params.toArray(paramArr);
-			
-	        jdbcTemplate.update(sql+where.toString(),paramArr);
-		}else{
+
+			jdbcTemplate.update(sql + where.toString(), paramArr);
+		} else {
 			log.warn("tagID and userGroupID are null");
 		}
 	}
@@ -55,18 +52,18 @@ public class TagGroupRelationDao extends SpringBaseDao<TagGroupRelation> {
 	public String getKey() {
 		return KEY;
 	}
-	
+
 	public TagGroupRelation findByTagIDAndUserGroupID(Long tagID, Long userGroupID) {
-		if(tagID!=null && userGroupID!=null){
-			String sql = "SELECT * FROM " + this.getTableName() + " WHERE "+ TagGroupRelation.TAG_ID +"=? AND "+ TagGroupRelation.USER_GROUP_ID + "=?";
-			List<TagGroupRelation> list= jdbcTemplate.query(sql,new Object[]{tagID,userGroupID},getRowMapper());
-	        if(list.size() > 0){
-	        	return list.get(0);
-	        }
+		if (tagID != null && userGroupID != null) {
+			String sql = "SELECT * FROM " + this.getTableName() + " WHERE " + TagGroupRelation.TAG_ID + "=? AND " + TagGroupRelation.USER_GROUP_ID + "=?";
+			List<TagGroupRelation> list = jdbcTemplate.query(sql, new Object[]{tagID, userGroupID}, getRowMapper());
+			if (list.size() > 0) {
+				return list.get(0);
+			}
 		}
-        return null;
-    }
-	
+		return null;
+	}
+
 	public List<TagGroupRelation> findByUserGroupID(Long userGroupID) {
 		return super.findBySingleField(TagGroupRelation.USER_GROUP_ID, userGroupID);
 	}
