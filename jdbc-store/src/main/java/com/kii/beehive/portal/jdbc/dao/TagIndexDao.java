f7b9d45e439a80f437e73389d92f7358f954fee8
hdchen <hdchen.tw@gmail.com>
2016-03-28 10:40:41 +0800

    #196 format source

--- jdbc-store/src/main/java/com/kii/beehive/portal/jdbc/dao/TagIndexDao.java
+++ jdbc-store/src/main/java/com/kii/beehive/portal/jdbc/dao/TagIndexDao.java
@@ -13,219 +13,219 @@ import java.util.List;
 @Repository
 public class TagIndexDao extends SpringBaseDao<TagIndex> {
 
-    //private Logger log= LoggerFactory.getLogger(TagIndexDao.class);
+	//private Logger log= LoggerFactory.getLogger(TagIndexDao.class);
 
-    public static final String TABLE_NAME = "tag_index";
-    public static final String KEY = "tag_id";
+	public static final String TABLE_NAME = "tag_index";
+	public static final String KEY = "tag_id";
 
-    private static final String SQL_FIND_USER_TAG = "SELECT * FROM " + TABLE_NAME + " WHERE " + TagIndex.CREATE_BY +
-            " = ?";
+	private static final String SQL_FIND_USER_TAG = "SELECT * FROM " + TABLE_NAME + " WHERE " + TagIndex.CREATE_BY +
+			" = ?";
 
-    private static final String SQL_FIND_USER_LOCATION = "SELECT t." + TagIndex.DISPLAY_NAME + " FROM " + TABLE_NAME + "" +
-            " t WHERE " + TagIndex.CREATE_BY + " = ?";
+	private static final String SQL_FIND_USER_LOCATION = "SELECT t." + TagIndex.DISPLAY_NAME + " FROM " + TABLE_NAME + "" +
+			" t WHERE " + TagIndex.CREATE_BY + " = ?";
 
-    /**
-     * find tag list by tagType and displayName
-     *
-     * @param tagType
-     * @param displayName if it's null or empty, will query all displayName under the tagType
-     * @return
-     */
-    public List<TagIndex> findTag(Long tagID, String tagType, String displayName) {
-        List<Object> params = new ArrayList<Object>();
-        StringBuilder sql = new StringBuilder();
-        sql.append("SELECT t.* FROM " + this.getTableName() + " t "
-                + " LEFT JOIN rel_tag_group rt ON t.tag_id = rt.tag_id "
-                + " LEFT JOIN user_group u ON u.user_group_id = rt.user_group_id "
-                + " LEFT JOIN rel_group_user rg ON u.user_group_id = rg.user_group_id  ");
+	/**
+	 * find tag list by tagType and displayName
+	 *
+	 * @param tagType
+	 * @param displayName if it's null or empty, will query all displayName under the tagType
+	 * @return
+	 */
+	public List<TagIndex> findTag(Long tagID, String tagType, String displayName) {
+		List<Object> params = new ArrayList<Object>();
+		StringBuilder sql = new StringBuilder();
+		sql.append("SELECT t.* FROM " + this.getTableName() + " t "
+				+ " LEFT JOIN rel_tag_group rt ON t.tag_id = rt.tag_id "
+				+ " LEFT JOIN user_group u ON u.user_group_id = rt.user_group_id "
+				+ " LEFT JOIN rel_group_user rg ON u.user_group_id = rg.user_group_id  ");
 
-        StringBuilder where = new StringBuilder();
-        where.append(" WHERE (rg.user_id = ? OR t.create_by=?)");
-        params.add(AuthInfoStore.getUserID());
-        params.add(AuthInfoStore.getUserID());
+		StringBuilder where = new StringBuilder();
+		where.append(" WHERE (rg.user_id = ? OR t.create_by=?)");
+		params.add(AuthInfoStore.getUserID());
+		params.add(AuthInfoStore.getUserID());
 
-        if (tagID != null) {
-            where.append(" AND t.").append(TagIndex.TAG_ID).append(" = ? ");
-            params.add(tagID);
-        }
+		if (tagID != null) {
+			where.append(" AND t.").append(TagIndex.TAG_ID).append(" = ? ");
+			params.add(tagID);
+		}
 
-        if (!Strings.isBlank(tagType)) {
-            where.append(" AND t.").append(TagIndex.TAG_TYPE).append(" = ? ");
-            params.add(tagType);
-        }
+		if (!Strings.isBlank(tagType)) {
+			where.append(" AND t.").append(TagIndex.TAG_TYPE).append(" = ? ");
+			params.add(tagType);
+		}
 
-        if (!Strings.isBlank(displayName)) {
-            where.append(" AND t.").append(TagIndex.DISPLAY_NAME).append(" = ? ");
-            params.add(displayName);
-        }
+		if (!Strings.isBlank(displayName)) {
+			where.append(" AND t.").append(TagIndex.DISPLAY_NAME).append(" = ? ");
+			params.add(displayName);
+		}
 
-        sql.append(where);
-        List<TagIndex> rows = jdbcTemplate.query(sql.toString(), params.toArray(new Object[params.size()]), getRowMapper());
-        return rows;
-    }
+		sql.append(where);
+		List<TagIndex> rows = jdbcTemplate.query(sql.toString(), params.toArray(new Object[params.size()]), getRowMapper());
+		return rows;
+	}
 
-    public List<TagIndex> findUserTagByTypeAndName(String userId, String tagType, String displayName) {
-        List<Object> params = new ArrayList<>();
-        params.add(userId);
-        StringBuilder sb = new StringBuilder(SQL_FIND_USER_TAG);
-        if (!Strings.isBlank(tagType)) {
-            sb.append(" AND ");
-            sb.append(TagIndex.TAG_TYPE);
-            sb.append(" = ?");
-            params.add(tagType);
-        }
-        if (!Strings.isBlank(displayName)) {
-            sb.append(" AND ");
-            sb.append(TagIndex.DISPLAY_NAME);
-            sb.append(" = ?");
-            params.add(displayName);
-        }
-        return jdbcTemplate.query(sb.toString(), params.toArray(new Object[]{}), getRowMapper());
-    }
+	public List<TagIndex> findUserTagByTypeAndName(String userId, String tagType, String displayName) {
+		List<Object> params = new ArrayList<>();
+		params.add(userId);
+		StringBuilder sb = new StringBuilder(SQL_FIND_USER_TAG);
+		if (!Strings.isBlank(tagType)) {
+			sb.append(" AND ");
+			sb.append(TagIndex.TAG_TYPE);
+			sb.append(" = ?");
+			params.add(tagType);
+		}
+		if (!Strings.isBlank(displayName)) {
+			sb.append(" AND ");
+			sb.append(TagIndex.DISPLAY_NAME);
+			sb.append(" = ?");
+			params.add(displayName);
+		}
+		return jdbcTemplate.query(sb.toString(), params.toArray(new Object[]{}), getRowMapper());
+	}
 
-    /**
-     * find tag list by tagType and displayName
-     *
-     * @param tagType
-     * @param displayName if it's null or empty, will query all displayName under the tagType
-     * @return
-     */
-    public List<TagIndex> findTagByTagTypeAndName(String tagType, String displayName) {
-        List<Object> params = new ArrayList<Object>();
-        StringBuilder sql = new StringBuilder();
-        sql.append("SELECT t.*, COUNT(r.thing_id) count, GROUP_CONCAT(r.thing_id) things "
-                + "FROM " + this.getTableName() + " t "
-                + "LEFT JOIN rel_thing_tag r ON r.tag_id = t.tag_id ");
+	/**
+	 * find tag list by tagType and displayName
+	 *
+	 * @param tagType
+	 * @param displayName if it's null or empty, will query all displayName under the tagType
+	 * @return
+	 */
+	public List<TagIndex> findTagByTagTypeAndName(String tagType, String displayName) {
+		List<Object> params = new ArrayList<Object>();
+		StringBuilder sql = new StringBuilder();
+		sql.append("SELECT t.*, COUNT(r.thing_id) count, GROUP_CONCAT(r.thing_id) things "
+				+ "FROM " + this.getTableName() + " t "
+				+ "LEFT JOIN rel_thing_tag r ON r.tag_id = t.tag_id ");
 
-        StringBuilder where = new StringBuilder();
-        if (AuthInfoStore.getTeamID() != null) {
-            sql.append(" INNER JOIN rel_team_tag rt ON t.tag_id=rt.tag_id ");
-            where.append(" rt.team_id = ? ");
-            params.add(AuthInfoStore.getTeamID());
-        }
+		StringBuilder where = new StringBuilder();
+		if (AuthInfoStore.getTeamID() != null) {
+			sql.append(" INNER JOIN rel_team_tag rt ON t.tag_id=rt.tag_id ");
+			where.append(" rt.team_id = ? ");
+			params.add(AuthInfoStore.getTeamID());
+		}
 
-        if (!Strings.isBlank(tagType)) {
-            if (where.length() > 0) {
-                where.append(" AND ");
-            }
-            where.append("t.").append(TagIndex.TAG_TYPE).append(" = ? ");
-            params.add(tagType);
-        }
+		if (!Strings.isBlank(tagType)) {
+			if (where.length() > 0) {
+				where.append(" AND ");
+			}
+			where.append("t.").append(TagIndex.TAG_TYPE).append(" = ? ");
+			params.add(tagType);
+		}
 
-        if (!Strings.isBlank(displayName)) {
-            if (where.length() > 0) {
-                where.append(" AND ");
-            }
-            where.append("t.").append(TagIndex.DISPLAY_NAME).append(" = ? ");
-            params.add(displayName);
-        }
-        if (where.length() > 0) {
-            where.insert(0, " WHERE ");
-        }
+		if (!Strings.isBlank(displayName)) {
+			if (where.length() > 0) {
+				where.append(" AND ");
+			}
+			where.append("t.").append(TagIndex.DISPLAY_NAME).append(" = ? ");
+			params.add(displayName);
+		}
+		if (where.length() > 0) {
+			where.insert(0, " WHERE ");
+		}
 
-        where.append("GROUP BY t.").append(TagIndex.TAG_ID);
-        sql.append(where);
+		where.append("GROUP BY t.").append(TagIndex.TAG_ID);
+		sql.append(where);
 
-        List<TagIndex> rows = jdbcTemplate.query(sql.toString(), params.toArray(new Object[params.size()]), getRowMapper());
-        return rows;
-    }
+		List<TagIndex> rows = jdbcTemplate.query(sql.toString(), params.toArray(new Object[params.size()]), getRowMapper());
+		return rows;
+	}
 
-    public List<String> findUserLocations(String userId, String parentLocation) {
-        List<Object> params = new ArrayList<>();
-        params.add(userId);
-        StringBuilder sb = new StringBuilder(SQL_FIND_USER_LOCATION);
-        if (!Strings.isBlank(parentLocation)) {
-            sb.append(" AND t.").append(TagIndex.DISPLAY_NAME).append(" LIKE ?");
-            params.add(parentLocation + '%');
-        }
-        sb.append(" ORDER BY t.").append(TagIndex.DISPLAY_NAME);
-        return jdbcTemplate.queryForList(sb.toString(), params.toArray(new Object[]{}), String.class);
-    }
+	public List<String> findUserLocations(String userId, String parentLocation) {
+		List<Object> params = new ArrayList<>();
+		params.add(userId);
+		StringBuilder sb = new StringBuilder(SQL_FIND_USER_LOCATION);
+		if (!Strings.isBlank(parentLocation)) {
+			sb.append(" AND t.").append(TagIndex.DISPLAY_NAME).append(" LIKE ?");
+			params.add(parentLocation + '%');
+		}
+		sb.append(" ORDER BY t.").append(TagIndex.DISPLAY_NAME);
+		return jdbcTemplate.queryForList(sb.toString(), params.toArray(new Object[]{}), String.class);
+	}
 
-    public List<String> findLocations(String parentLocation) {
+	public List<String> findLocations(String parentLocation) {
 
-        StringBuffer sql = new StringBuffer();
-        sql.append("SELECT t.").append(TagIndex.DISPLAY_NAME).append(" FROM ").append(this.getTableName() + " t ");
+		StringBuffer sql = new StringBuffer();
+		sql.append("SELECT t.").append(TagIndex.DISPLAY_NAME).append(" FROM ").append(this.getTableName() + " t ");
 
-        StringBuffer where = new StringBuffer();
-        where.append(" WHERE t.").append(TagIndex.TAG_TYPE).append(" = ? ")
-                .append(" AND t.").append(TagIndex.DISPLAY_NAME).append(" like ? ");
-
-
-        if (AuthInfoStore.getTeamID() != null) {
-            sql.append(" INNER JOIN rel_team_tag rt ON t.tag_id=rt.tag_id ");
-            where.append("AND rt.team_id = ").append(AuthInfoStore.getTeamID());
-        }
-
-        where.append(" ORDER BY t.").append(TagIndex.DISPLAY_NAME);
-        sql.append(where);
-
-        Object[] params = new Object[]{TagType.Location.toString(), parentLocation + "%"};
-        List<String> rows = jdbcTemplate.queryForList(sql.toString(), params, String.class);
-
-        return rows;
-    }
-
-    public List<TagIndex> findTagByFullTagName(String fullTagName) {
-        List<Object> params = new ArrayList<Object>();
-        StringBuilder sql = new StringBuilder();
-        sql.append("SELECT t.* "
-                + "FROM " + this.getTableName() +" t ");
-
-        StringBuilder where = new StringBuilder();
-        if(AuthInfoStore.getTeamID() != null){
-            sql.append(" INNER JOIN rel_team_tag rt ON t.tag_id=rt.tag_id ");
-            where.append(" rt.team_id = ? ");
-            params.add(AuthInfoStore.getTeamID());
-        }
-
-        if(!Strings.isBlank(fullTagName)){
-            if(where.length() > 0){
-                where.append(" AND ");
-            }
-            where.append("t.").append(TagIndex.FULL_TAG_NAME).append(" = ? ");
-            params.add(fullTagName);
-        }
-
-        if(where.length() > 0){
-            where.insert(0, " WHERE ");
-        }
-
-        sql.append(where);
-
-        List<TagIndex> rows = jdbcTemplate.query(sql.toString(), params.toArray(new Object[params.size()]), getRowMapper());
-        return rows;
-    }
+		StringBuffer where = new StringBuffer();
+		where.append(" WHERE t.").append(TagIndex.TAG_TYPE).append(" = ? ")
+				.append(" AND t.").append(TagIndex.DISPLAY_NAME).append(" like ? ");
 
 
-    /**
-     * get the list of tags related to the thing
-     *
-     * @param globalThingID
-     * @return
-     */
-    public List<TagIndex> findTagByGlobalThingID(Long globalThingID) {
-        StringBuffer sql = new StringBuffer();
-        sql.append("SELECT t.*")
-                .append(" FROM ")
-                .append(TagIndexDao.TABLE_NAME).append(" t, ")
-                .append(" ( SELECT ").append(TagThingRelation.TAG_ID)
-                .append("     FROM ").append(TagThingRelationDao.TABLE_NAME)
-                .append("    WHERE ").append(TagThingRelation.THING_ID).append("=? ) r ")
-                .append(" WHERE r.").append(TagThingRelation.TAG_ID).append("=t.").append(TagIndex.TAG_ID);
+		if (AuthInfoStore.getTeamID() != null) {
+			sql.append(" INNER JOIN rel_team_tag rt ON t.tag_id=rt.tag_id ");
+			where.append("AND rt.team_id = ").append(AuthInfoStore.getTeamID());
+		}
 
-        List<TagIndex> rows = jdbcTemplate.query(sql.toString(), new Object[]{globalThingID}, getRowMapper());
-        return rows;
-    }
+		where.append(" ORDER BY t.").append(TagIndex.DISPLAY_NAME);
+		sql.append(where);
 
-    @Override
-    public String getTableName() {
-        return TABLE_NAME;
-    }
+		Object[] params = new Object[]{TagType.Location.toString(), parentLocation + "%"};
+		List<String> rows = jdbcTemplate.queryForList(sql.toString(), params, String.class);
+
+		return rows;
+	}
+
+	public List<TagIndex> findTagByFullTagName(String fullTagName) {
+		List<Object> params = new ArrayList<Object>();
+		StringBuilder sql = new StringBuilder();
+		sql.append("SELECT t.* "
+				+ "FROM " + this.getTableName() + " t ");
+
+		StringBuilder where = new StringBuilder();
+		if (AuthInfoStore.getTeamID() != null) {
+			sql.append(" INNER JOIN rel_team_tag rt ON t.tag_id=rt.tag_id ");
+			where.append(" rt.team_id = ? ");
+			params.add(AuthInfoStore.getTeamID());
+		}
+
+		if (!Strings.isBlank(fullTagName)) {
+			if (where.length() > 0) {
+				where.append(" AND ");
+			}
+			where.append("t.").append(TagIndex.FULL_TAG_NAME).append(" = ? ");
+			params.add(fullTagName);
+		}
+
+		if (where.length() > 0) {
+			where.insert(0, " WHERE ");
+		}
+
+		sql.append(where);
+
+		List<TagIndex> rows = jdbcTemplate.query(sql.toString(), params.toArray(new Object[params.size()]), getRowMapper());
+		return rows;
+	}
 
 
-    @Override
-    public String getKey() {
-        return KEY;
-    }
+	/**
+	 * get the list of tags related to the thing
+	 *
+	 * @param globalThingID
+	 * @return
+	 */
+	public List<TagIndex> findTagByGlobalThingID(Long globalThingID) {
+		StringBuffer sql = new StringBuffer();
+		sql.append("SELECT t.*")
+				.append(" FROM ")
+				.append(TagIndexDao.TABLE_NAME).append(" t, ")
+				.append(" ( SELECT ").append(TagThingRelation.TAG_ID)
+				.append("     FROM ").append(TagThingRelationDao.TABLE_NAME)
+				.append("    WHERE ").append(TagThingRelation.THING_ID).append("=? ) r ")
+				.append(" WHERE r.").append(TagThingRelation.TAG_ID).append("=t.").append(TagIndex.TAG_ID);
+
+		List<TagIndex> rows = jdbcTemplate.query(sql.toString(), new Object[]{globalThingID}, getRowMapper());
+		return rows;
+	}
+
+	@Override
+	public String getTableName() {
+		return TABLE_NAME;
+	}
+
+
+	@Override
+	public String getKey() {
+		return KEY;
+	}
 }
