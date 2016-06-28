e9308a63e7035b56a70822d2373d4eca86ff8463
hdchen <hongdian.chen@kii.com>
2016-06-28 17:05:09 +0800

    #292 Missing header Access-Control-Allow-Origin

--- web-mvc/src/main/java/com/kii/beehive/portal/web/config/WebMvcConfig.java
+++ web-mvc/src/main/java/com/kii/beehive/portal/web/config/WebMvcConfig.java
@@ -2,7 +2,6 @@ package com.kii.beehive.portal.web.config;
 
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -11,13 +10,9 @@ import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 import com.kii.beehive.portal.web.controller.STOMPMessageController;
-import com.kii.beehive.portal.web.help.AuthInterceptor;
 
 @EnableWebMvc
 @Configuration
@@ -27,9 +22,6 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
 	@Autowired
 	private ObjectMapper mapper;
-
-	@Autowired
-	private AuthInterceptor  authInterceptor;
 
 	@Override
 	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
@@ -44,12 +36,6 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 	}
 
 
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-
-		registry.addInterceptor(authInterceptor).addPathPatterns("/**");
-
-	}
 	private HttpMessageConverter<Object> createJsonMessageConverter() {
 
 		MappingJackson2HttpMessageConverter jsonMarshaller = new MappingJackson2HttpMessageConverter();
