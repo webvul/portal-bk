b992fdd743570d1486c91d66fe9ec902e8df4976
JasonChang <jason.chang@kii.com>
2016-07-06 17:33:04 +0800

    refactor report api

--- store-service/src/main/java/com/kii/beehive/business/factory/ESTaskFactory.java
+++ store-service/src/main/java/com/kii/beehive/business/factory/ESTaskFactory.java
@@ -1,13 +1,18 @@
 package com.kii.beehive.business.factory;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.kii.beehive.business.elasticsearch.task.BulkUploadTask;
+import com.kii.beehive.business.elasticsearch.task.SearchTask;
+import org.elasticsearch.action.search.SearchType;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.search.aggregations.AggregationBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import javax.annotation.PreDestroy;
 import java.io.IOException;
 import java.util.List;
-import org.elasticsearch.client.Client;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.kii.beehive.business.elasticsearch.task.BulkUploadTask;
 
 /**
  * Created by hdchen on 7/1/16.
@@ -37,6 +42,20 @@ public class ESTaskFactory {
 		task.setIndex_name(index);
 		task.setType_name(type);
 		task.setDocs(documents);
+		return task;
+	}
+
+	public SearchTask getSearchTask(String index, String type, SearchType searchType, QueryBuilder queryBuilder,
+									AggregationBuilder aggregationBuilder, int size, int from) {
+		SearchTask task = new SearchTask();
+		task.setClient(indexOpClient);
+		task.setIndex_name(index);
+		task.setType_name(type);
+		task.setSearchType(searchType);
+		task.setSize(size);
+		task.setFrom(from);
+		task.setQueryBuilder(queryBuilder);
+		task.setAggregationBuilder(aggregationBuilder);
 		return task;
 	}
 }
--- web-mvc/src/main/java/com/kii/beehive/portal/web/controller/ESServiceController.java
+++ web-mvc/src/main/java/com/kii/beehive/portal/web/controller/ESServiceController.java
@@ -1,14 +1,20 @@
 package com.kii.beehive.portal.web.controller;
 
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.kii.beehive.business.elasticsearch.TaskManager;
+import com.kii.beehive.business.manager.TagThingManager;
+import com.kii.beehive.portal.auth.AuthInfoStore;
+import com.kii.beehive.portal.exception.EntryNotFoundException;
+import com.kii.beehive.portal.jdbc.entity.GlobalThingInfo;
+import com.kii.beehive.portal.web.constant.Constants;
+import com.kii.beehive.portal.web.entity.SearchRestBean;
+import com.kii.beehive.portal.web.exception.PortalException;
+import org.apache.logging.log4j.util.Strings;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * Created by hdchen on 6/30/16.
@@ -21,6 +27,9 @@ public class ESServiceController {
 	@Autowired
 	private TaskManager transportClientManager;
 
+	@Autowired
+	protected TagThingManager thingTagManager;
+
 	/**
 	 * POST /es/bulkUpload/{appId}/{vendorThingId}
 	 *
@@ -32,5 +41,76 @@ public class ESServiceController {
 						   @PathVariable("vendorThingId") String vendorThingId,
 						   @RequestBody List<JsonNode> documents) {
 		transportClientManager.bulkUpload(appId, vendorThingId, documents);
+	}
+
+
+	/**
+	 * POST /es/historical/aggregate
+	 *
+	 * @return
+	 */
+	@RequestMapping(value = "/historical/aggregate", method = {RequestMethod.POST})
+	public String aggregate(@RequestBody SearchRestBean searchRestBean) {
+
+		if (Strings.isBlank(searchRestBean.getVendorThingID()) || Strings.isBlank(searchRestBean.getIntervalField())
+				|| Strings.isBlank(searchRestBean.getOperatorField())
+				|| searchRestBean.getStartDate() == null || searchRestBean.getEndDate() == null
+				|| searchRestBean.getFields() == null || searchRestBean.getFields().length == 0
+				|| searchRestBean.getUnit() == 0) {
+			throw new PortalException("RequiredFieldsMissing", HttpStatus.BAD_REQUEST);
+		}
+
+		GlobalThingInfo thing = thingTagManager.getThingsByVendorThingId(searchRestBean.getVendorThingID());
+
+		if (thing == null) {
+			throw EntryNotFoundException.thingNotFound(searchRestBean.getVendorThingID());
+		}
+		if (!Constants.ADMIN_ID.equals(AuthInfoStore.getUserID())) {//non-admin
+			thingTagManager.getAccessibleThingById(AuthInfoStore.getUserID(), thing.getId());
+		}
+
+		String kiiThingID = "thing:" + thing.getFullKiiThingID().substring(thing.getFullKiiThingID().indexOf("-") + 1);
+
+		String r = transportClientManager.queryBuilderForAggs(thing.getKiiAppID(), "spark",
+				kiiThingID,
+				searchRestBean.getStartDate(), searchRestBean.getEndDate(), searchRestBean.getIntervalField(),
+				searchRestBean.getUnit(), searchRestBean.getOperatorField(), searchRestBean.getFields());
+
+		return r;
+	}
+
+	/**
+	 * POST /es/historical
+	 *
+	 * @param searchRestBean
+	 * @return
+	 */
+	@RequestMapping(value = "/historical", method = {RequestMethod.POST})
+	public String historical(@RequestBody SearchRestBean searchRestBean) {
+		if (Strings.isBlank(searchRestBean.getVendorThingID())
+				|| searchRestBean.getStartDate() == null || searchRestBean.getEndDate() == null
+				|| searchRestBean.getSize() == 0) {
+			throw new PortalException("RequiredFieldsMissing", HttpStatus.BAD_REQUEST);
+		}
+
+		if (searchRestBean.getSize() > 100) {
+			searchRestBean.setSize(100);
+		}
+
+		GlobalThingInfo thing = thingTagManager.getThingsByVendorThingId(searchRestBean.getVendorThingID());
+
+		if (thing == null) {
+			throw EntryNotFoundException.thingNotFound(searchRestBean.getVendorThingID());
+		}
+		if (!Constants.ADMIN_ID.equals(AuthInfoStore.getUserID())) {//non-admin
+			thingTagManager.getAccessibleThingById(AuthInfoStore.getUserID(), thing.getId());
+		}
+
+		String kiiThingID = "thing:" + thing.getFullKiiThingID().substring(thing.getFullKiiThingID().indexOf("-") + 1);
+
+		String result = transportClientManager.queryBuilderForHistorical(thing.getKiiAppID(), "spark", kiiThingID,
+				searchRestBean.getStartDate(),
+				searchRestBean.getEndDate(), searchRestBean.getSize(), searchRestBean.getFrom());
+		return result;
 	}
 }
