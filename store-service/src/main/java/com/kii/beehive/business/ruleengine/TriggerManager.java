b8b845af31bd4350a984c634618c4e4f04a86957
steven.jiang@kii.com <steven.jiang@kii.com>
2016-08-05 13:23:59 +0800

    optimize  imports

--- store-service/src/main/java/com/kii/beehive/business/ruleengine/TriggerManager.java
+++ store-service/src/main/java/com/kii/beehive/business/ruleengine/TriggerManager.java
@@ -1,6 +1,7 @@
 package com.kii.beehive.business.ruleengine;
 
 import javax.annotation.PostConstruct;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -8,12 +9,15 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 import com.kii.beehive.business.event.BusinessEventListenerService;
 import com.kii.beehive.business.helper.TriggerCreator;
 import com.kii.beehive.business.manager.AppInfoManager;
