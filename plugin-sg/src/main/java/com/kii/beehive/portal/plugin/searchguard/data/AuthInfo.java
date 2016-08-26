b30de06a446ddc135b30e990a3c0137f057aaa72
hdchen <hongdian.chen@kii.com>
2016-08-26 15:08:15 +0800

    Update AuthInfo

--- plugin-sg/src/main/java/com/kii/beehive/portal/plugin/searchguard/data/AuthInfo.java
+++ plugin-sg/src/main/java/com/kii/beehive/portal/plugin/searchguard/data/AuthInfo.java
@@ -1,22 +1,53 @@
 package com.kii.beehive.portal.plugin.searchguard.data;
 
+import java.util.Date;
+import java.util.Set;
+
 /**
  * Created by hdchen on 8/25/16.
  */
 public class AuthInfo {
 	private Long id;
 
+	private Date createDate;
+
+	private Date modifyDate;
+
+	private String createBy;
+
+	private String modifyBy;
+
+	private Boolean isDeleted;
+
+	private String kiiUserID;
+
 	private String userName;
+
+	private String phone;
+
+	private String mail;
+
+	private String displayName;
 
 	private String roleName;
 
+	private String activityToken;
+
+	private String userPassword;
+
 	private String userID;
 
-	private Long faceSubjectId;
+	private Integer faceSubjectId; // face++
 
 	private Boolean enable;
 
 	private String accessToken;
+
+	private Set<String> permissions;
+
+	private Long teamID;
+
+	private String teamName;
 
 	public Long getId() {
 		return id;
@@ -24,6 +55,54 @@ public class AuthInfo {
 
 	public void setId(Long id) {
 		this.id = id;
+	}
+
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+
+	public Date getModifyDate() {
+		return modifyDate;
+	}
+
+	public void setModifyDate(Date modifyDate) {
+		this.modifyDate = modifyDate;
+	}
+
+	public String getCreateBy() {
+		return createBy;
+	}
+
+	public void setCreateBy(String createBy) {
+		this.createBy = createBy;
+	}
+
+	public String getModifyBy() {
+		return modifyBy;
+	}
+
+	public void setModifyBy(String modifyBy) {
+		this.modifyBy = modifyBy;
+	}
+
+	public Boolean getDeleted() {
+		return isDeleted;
+	}
+
+	public void setDeleted(Boolean deleted) {
+		isDeleted = deleted;
+	}
+
+	public String getKiiUserID() {
+		return kiiUserID;
+	}
+
+	public void setKiiUserID(String kiiUserID) {
+		this.kiiUserID = kiiUserID;
 	}
 
 	public String getUserName() {
@@ -34,12 +113,52 @@ public class AuthInfo {
 		this.userName = userName;
 	}
 
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getDisplayName() {
+		return displayName;
+	}
+
+	public void setDisplayName(String displayName) {
+		this.displayName = displayName;
+	}
+
 	public String getRoleName() {
 		return roleName;
 	}
 
 	public void setRoleName(String roleName) {
 		this.roleName = roleName;
+	}
+
+	public String getActivityToken() {
+		return activityToken;
+	}
+
+	public void setActivityToken(String activityToken) {
+		this.activityToken = activityToken;
+	}
+
+	public String getUserPassword() {
+		return userPassword;
+	}
+
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
 	}
 
 	public String getUserID() {
@@ -50,11 +169,11 @@ public class AuthInfo {
 		this.userID = userID;
 	}
 
-	public Long getFaceSubjectId() {
+	public Integer getFaceSubjectId() {
 		return faceSubjectId;
 	}
 
-	public void setFaceSubjectId(Long faceSubjectId) {
+	public void setFaceSubjectId(Integer faceSubjectId) {
 		this.faceSubjectId = faceSubjectId;
 	}
 
@@ -72,5 +191,29 @@ public class AuthInfo {
 
 	public void setAccessToken(String accessToken) {
 		this.accessToken = accessToken;
+	}
+
+	public Set<String> getPermissions() {
+		return permissions;
+	}
+
+	public void setPermissions(Set<String> permissions) {
+		this.permissions = permissions;
+	}
+
+	public Long getTeamID() {
+		return teamID;
+	}
+
+	public void setTeamID(Long teamID) {
+		this.teamID = teamID;
+	}
+
+	public String getTeamName() {
+		return teamName;
+	}
+
+	public void setTeamName(String teamName) {
+		this.teamName = teamName;
 	}
 }
