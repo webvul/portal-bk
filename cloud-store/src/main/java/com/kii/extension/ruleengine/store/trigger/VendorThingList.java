e3a6ecd2ed3fe6c5a74ea37d536bc860e48aa60d
carlos.yang <carlos.yang@kii.com>
2016-09-07 15:21:37 +0800

    update trigger api

--- cloud-store/src/main/java/com/kii/extension/ruleengine/store/trigger/VendorThingList.java
+++ cloud-store/src/main/java/com/kii/extension/ruleengine/store/trigger/VendorThingList.java
@@ -8,6 +8,18 @@ public class VendorThingList {
 	private List<String> vendorThingIdList=new ArrayList<>();
 
 
+	private List<Long> thingList=new ArrayList<>();
+
+
+	public List<Long> getThingList() {
+		return thingList;
+	}
+
+	public void setThingList(List<Long> thingList) {
+		this.thingList = thingList;
+	}
+
+
 	public List<String> getVendorThingIdList() {
 		return vendorThingIdList;
 	}
