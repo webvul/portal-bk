747bcbc673f9fd83b52b7c4b51bd6bca763ea650
hdchen <hongdian.chen@kii.com>
2016-11-03 14:33:29 +0800

    Get resource from servlet context as a fallback

--- web-mvc/src/main/java/com/kii/beehive/portal/web/controller/ServerInfoController.java
+++ web-mvc/src/main/java/com/kii/beehive/portal/web/controller/ServerInfoController.java
@@ -1,5 +1,11 @@
 package com.kii.beehive.portal.web.controller;
 
+import javax.servlet.ServletContext;
+import java.io.IOException;
+import java.util.jar.Manifest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,8 +17,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/info", consumes = {MediaType.ALL_VALUE}, produces = {MediaType.ALL_VALUE})
 public class ServerInfoController {
+	private static final Logger LOG = LoggerFactory.getLogger(ServerInfoController.class);
+
+	@Autowired
+	private ServletContext context;
+
 	@RequestMapping(value = "/buildNum", method = {RequestMethod.GET})
 	public String buildNumber() {
-		return getClass().getPackage().getImplementationVersion();
+		final String version = getClass().getPackage().getImplementationVersion();
+		if (null != version) {
+			return version;
+		}
+
+		final Manifest manifest;
+		try {
+			manifest = new Manifest(context.getResourceAsStream("/META-INF/MANIFEST.MF"));
+			return manifest.getMainAttributes().getValue("Implementation-Version");
+		} catch (IOException e) {
+			LOG.info(e.getMessage(), e);
+			return null;
+		}
 	}
 }
