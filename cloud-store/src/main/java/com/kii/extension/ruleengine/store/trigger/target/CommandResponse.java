d76b771de8d7ee540e9cd8d2d6cb4cd9b01eb8e0
steven.jiang@kii.com <steven.jiang@kii.com>
2016-11-30 15:55:31 +0800

    modify ignore list

---
+++ cloud-store/src/main/java/com/kii/extension/ruleengine/store/trigger/target/CommandResponse.java
@@ -0,0 +1,70 @@
+package com.kii.extension.ruleengine.store.trigger.target;
+
+import com.kii.extension.sdk.entity.thingif.ThingCommand;
+
+public class CommandResponse extends  TriggerResult {
+
+
+
+	private String result;
+
+	private ThingCommand command;
+
+	private String kiiThingID;
+
+	private String appID;
+
+	private long thingID;
+
+	public CommandResponse(){
+
+	}
+
+
+	public ThingCommand getCommand() {
+		return command;
+	}
+
+	public void setCommand(ThingCommand command) {
+		this.command = command;
+	}
+
+	@Override
+	public String getType() {
+		return "command";
+	}
+
+
+
+	public String getResult() {
+		return result;
+	}
+
+	public void setResult(String result) {
+		this.result = result;
+	}
+
+	public String getKiiThingID() {
+		return kiiThingID;
+	}
+
+	public void setKiiThingID(String thingID) {
+		this.kiiThingID = thingID;
+	}
+
+	public long getThingID() {
+		return thingID;
+	}
+
+	public void setThingID(long thingID) {
+		this.thingID = thingID;
+	}
+
+	public String getAppID() {
+		return appID;
+	}
+
+	public void setAppID(String appID) {
+		this.appID = appID;
+	}
+}
