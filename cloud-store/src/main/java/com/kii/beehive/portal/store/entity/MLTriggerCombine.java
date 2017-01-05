9f7f2e1c8fcfcea00bf1762d0e34213caba8249f
steven.jiang@kii.com <steven.jiang@kii.com>
2017-01-05 16:14:16 +0800

    resolve conflict

--- cloud-store/src/main/java/com/kii/beehive/portal/store/entity/MLTriggerCombine.java
+++ cloud-store/src/main/java/com/kii/beehive/portal/store/entity/MLTriggerCombine.java
@@ -1,12 +1,18 @@
 package com.kii.beehive.portal.store.entity;
 
-import com.kii.beehive.portal.store.entity.trigger.TriggerRecord;
+import com.kii.extension.ruleengine.store.trigger.Condition;
+import com.kii.extension.ruleengine.store.trigger.TriggerRecord;
 
 public class MLTriggerCombine extends PortalEntity {
+	
+	
+	private Condition  mlCondition;
 	
 	private String mlTaskID;
 	
 	private TriggerRecord businessTrigger;
+	
+	private boolean joinWithAND=true;
 	
 	private String relationTriggerID;
 	
@@ -18,10 +24,15 @@ public class MLTriggerCombine extends PortalEntity {
 		this.relationTriggerID = relationTriggerID;
 	}
 	
-	public String getCombineTriggerID(){
-		return super.getId();
+
+	
+	public Condition getMlCondition() {
+		return mlCondition;
 	}
 	
+	public void setMlCondition(Condition mlCondition) {
+		this.mlCondition = mlCondition;
+	}
 	
 	public String getMlTaskID() {
 		return mlTaskID;
@@ -39,4 +50,11 @@ public class MLTriggerCombine extends PortalEntity {
 		this.businessTrigger = businessTrigger;
 	}
 	
+	public boolean isJoinWithAND() {
+		return joinWithAND;
+	}
+	
+	public void setJoinWithAND(boolean joinWithAND) {
+		this.joinWithAND = joinWithAND;
+	}
 }
