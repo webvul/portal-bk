95ca22c03637a5ea30d8e4b11e74129138d118b1
JasonChang <jason.chang@kii.com>
2017-01-19 18:24:52 +0800

    add external.dev super token

--- store-service/src/main/java/com/kii/beehive/portal/helper/AuthInfoService.java
+++ store-service/src/main/java/com/kii/beehive/portal/helper/AuthInfoService.java
@@ -1,82 +1,76 @@
 package com.kii.beehive.portal.helper;
 
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.stream.Collectors;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-
 import com.kii.beehive.portal.entitys.AuthInfo;
 import com.kii.beehive.portal.exception.TokenTimeoutException;
 import com.kii.beehive.portal.exception.UnauthorizedException;
 import com.kii.beehive.portal.jdbc.entity.BeehiveJdbcUser;
 import com.kii.extension.sdk.annotation.BindAppByName;
 import com.kii.extension.sdk.context.UserTokenBindTool;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 /**
  * this class queries the url permission on the given user/token and constructs AuthInfoEntry to store these info
- *
  */
 @Component
-@BindAppByName(appName="portal",appBindSource="propAppBindTool")
+@BindAppByName(appName = "portal", appBindSource = "propAppBindTool")
 public class AuthInfoService {
 
-    private Logger log= LoggerFactory.getLogger(AuthInfoService.class);
+	private Logger log = LoggerFactory.getLogger(AuthInfoService.class);
 
-	private Map<String,AuthInfo> userTokenMap=new ConcurrentHashMap<>();
+	private Map<String, AuthInfo> userTokenMap = new ConcurrentHashMap<>();
 
 
 	@Autowired
 	private UserTokenBindTool tokenBind;
-	
-	
+
+
 	@Value("${spring.profile}")
 	private String profile;
 
-	
-    public void createAuthInfoEntry(AuthInfo authInfo, String token) {
 
-        log.debug("createAuthInfoEntry token: " + token + " for userID: " + authInfo.getUserID());
+	public void createAuthInfoEntry(AuthInfo authInfo, String token) {
 
-		userTokenMap.put(token,authInfo);
+		log.debug("createAuthInfoEntry token: " + token + " for userID: " + authInfo.getUserID());
 
-        return;
-    }
+		userTokenMap.put(token, authInfo);
 
-
+		return;
+	}
 
 
-	public void bindUser(BeehiveJdbcUser user){
+	public void bindUser(BeehiveJdbcUser user) {
 
-		tokenBind.bindUserInfo(user.getUserID(),user.getUserPassword());
+		tokenBind.bindUserInfo(user.getUserID(), user.getUserPassword());
 
 	}
 
 
+	public AuthInfo getAuthInfoByToken(String token) {
 
-
-
-	public AuthInfo getAuthInfoByToken(String token){
-
-		if(("local".equals(profile)||
-				"internal.dev".equals(profile) ) && "super_token".equals(token)){
+		if (("local".equals(profile) ||
+				"internal.dev".equals(profile) ||
+				"external.dev".equals(profile)) && "super_token".equals(token)) {
 			AuthInfo info = new AuthInfo();
 			info.setUserID(0l);
 			return info;
 		}
 
-		AuthInfo info= userTokenMap.get(token);
+		AuthInfo info = userTokenMap.get(token);
 
-		if(info==null){
+		if (info == null) {
 			return null;
 		}
-		if(info.getExpireTime().getTime()<new Date().getTime()){
+		if (info.getExpireTime().getTime() < new Date().getTime()) {
 			userTokenMap.remove(token);
 			throw new TokenTimeoutException(token);
 		}
@@ -84,25 +78,25 @@ public class AuthInfoService {
 		return info;
 	}
 
-	public void removeToken(String token){
+	public void removeToken(String token) {
 
-		AuthInfo info=userTokenMap.remove(token);
+		AuthInfo info = userTokenMap.remove(token);
 
 
-		if(info==null){
-			throw  new UnauthorizedException(UnauthorizedException.LOGIN_TOKEN_INVALID,"token",token);
+		if (info == null) {
+			throw new UnauthorizedException(UnauthorizedException.LOGIN_TOKEN_INVALID, "token", token);
 		}
 
 	}
-	
-	
+
+
 	public void removeTokenByUserID(String id) {
 
-		List<String> tokens=userTokenMap.entrySet().stream().filter((entry)->{
-					AuthInfo info=entry.getValue();
-					return !info.is3Party()&&info.getUserID().equals(id);
-				})
-				.map((entry)->entry.getKey()).collect(Collectors.toList());
+		List<String> tokens = userTokenMap.entrySet().stream().filter((entry) -> {
+			AuthInfo info = entry.getValue();
+			return !info.is3Party() && info.getUserID().equals(id);
+		})
+				.map((entry) -> entry.getKey()).collect(Collectors.toList());
 
 		userTokenMap.keySet().removeAll(tokens);
 
